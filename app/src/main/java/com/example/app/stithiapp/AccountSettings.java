6515ebfe5fac80046f6b63697954d8e5ed256879
deepdiwakar9 <31311858+deepdiwakar9@users.noreply.github.com>
2018-06-22 23:05:48 +0530

    Profile Image

--- app/src/main/java/com/example/app/stithiapp/AccountSettings.java
+++ app/src/main/java/com/example/app/stithiapp/AccountSettings.java
@@ -1,29 +1,49 @@
 package com.example.app.stithiapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AccountSettings extends AppCompatActivity {
 
+    private ProgressDialog mProgressDialog;
     private TextView mName, mStatus;
-    private Button statusButton;
+    private Button statusButton, changeImage;
+    private CircleImageView mImage;
 
     private String currentUser;
 
     private DatabaseReference userDatabse;
+    private StorageReference mProfileStorage;
+
+    private static final int GALLERY_REQ = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,22 +52,30 @@ public class AccountSettings extends AppCompatActivity {
 
         mName = findViewById(R.id.settings_display_name);
         mStatus = findViewById(R.id.settings_status);
+        mImage = findViewById(R.id.profile_image);
         statusButton = findViewById(R.id.settings_status_btn);
+        changeImage = findViewById(R.id.settings_change_image);
 
-        currentUser = getIntent().getStringExtra("USERID");
-        Log.d("NAME", currentUser);
+        currentUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         userDatabse = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUser);
+        mProfileStorage = FirebaseStorage.getInstance().getReference().child("profile_images");
 
         userDatabse.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String name = dataSnapshot.child("name").getValue().toString();
                 String status = dataSnapshot.child("status").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+                String defaultString = "default";
                 Log.d("NAME", name);
 
                 mName.setText(name);
                 mStatus.setText(status);
+                if(!image.equals(defaultString)){
+                    Picasso.get().load(image).into(mImage);
+                }
+
             }
 
             @Override
@@ -65,5 +93,66 @@ public class AccountSettings extends AppCompatActivity {
                 startActivity(statusIntent);
             }
         });
+
+        changeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+
+                startActivityForResult(galleryIntent, GALLERY_REQ);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQ && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                mProgressDialog = new ProgressDialog(AccountSettings.this);
+                mProgressDialog.setTitle("Uploading Image...");
+                mProgressDialog.setMessage("Please wait while we upload and process the image.");
+                mProgressDialog.setCanceledOnTouchOutside(false);
+                mProgressDialog.show();
+
+                Uri resultUri = result.getUri();
+                StorageReference filepath = mProfileStorage.child(currentUser + ".jpg");
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            String downloadUrl = task.getResult().getDownloadUrl().toString();
+                            userDatabse.child("image").setValue(downloadUrl).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    mProgressDialog.dismiss();
+                                    Toast.makeText(getApplicationContext(), "Successfully Uploaded", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }else{
+                            mProgressDialog.hide();
+                            Toast.makeText(getApplicationContext(), "Error while uploading", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/app/stithiapp/StatusInputActivity.java
+++ app/src/main/java/com/example/app/stithiapp/StatusInputActivity.java
@@ -1,17 +1,24 @@
 package com.example.app.stithiapp;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class StatusInputActivity extends AppCompatActivity {
 
+    ProgressDialog statusDialog;
     Toolbar statusToolbar;
     TextInputLayout statusInput;
     String status, userID;
@@ -41,8 +48,21 @@ public class StatusInputActivity extends AppCompatActivity {
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                statusDatabse.setValue(statusInput.getEditText().getText().toString());
-                finish();
+                statusDialog = new ProgressDialog(StatusInputActivity.this);
+                statusDialog.setTitle("Saving Changes!!");
+                statusDialog.setMessage("Please wait while we save the changes");
+                statusDialog.show();
+
+                statusDatabse.setValue(statusInput.getEditText().getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            statusDialog.dismiss();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"You got some Errors", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
